665ee227b666eaa92f9db60871ca56578ce0cd2c
CLacaile <clementlacaile@gmail.com>
2019-10-14 15:41:25 +0200

    initial commit

---
+++ ProjetStoch/src/model/Utils.java
@@ -0,0 +1,9 @@
+public class Utils {
+    public static int fact(int x) {
+        int product = 1;
+        for ( int j=1; j<=x; j++ ) {
+            product *= j;
+        }
+        return product;
+    }
+}
