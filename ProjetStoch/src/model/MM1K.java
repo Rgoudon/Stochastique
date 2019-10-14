e770eab901f3bd13123d72435361707803f8da7a
CLacaile <clementlacaile@gmail.com>
2019-10-14 14:26:57 +0200

    removed "this."

---
+++ ProjetStoch/src/model/MM1K.java
@@ -0,0 +1,41 @@
+package model;
+
+public class MM1K extends FileAttente {
+    
+
+    MM1K(int maxCustomer) {
+        super();
+        this.S = 1;
+        this.K = maxCustomer;
+    }
+
+    public float computeRho() {
+        rho = lambda/mu;
+        return rho;
+    }
+
+    public float computeNbCustomerInSystem() {
+        if (rho == 1) {
+            L = K/2;
+        }
+        else {
+            L = (float) (rho*(1 - (K+1)*Math.pow(rho, K) + K*Math.pow(rho, (K+1)))/
+                        ((1 - rho)*(1 - Math.pow(rho,(K+1)))));
+        }
+        return this.L;
+    }
+
+    public float computeNbCustomerInQueue() {
+        return (this.lambda*this.lambda)/(this.mu*(this.mu-this.lambda));
+    }
+
+    public float computeMeanTimeInSystem() {
+        return 1/(this.mu-this.lambda);
+    }
+
+    public float computeMeanTimeInQueue() {
+        return this.lambda/(this.mu*(this.mu-this.lambda));
+    }
+
+
+}
