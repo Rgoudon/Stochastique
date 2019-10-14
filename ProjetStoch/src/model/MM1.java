8f5f54e65c2ac79c800369c92381283840060951
CLacaile <clementlacaile@gmail.com>
2019-10-14 12:14:28 +0200

    added L,Lq,W,Wq computation functions

--- ProjetStoch/src/model/MM1.java
+++ ProjetStoch/src/model/MM1.java
@@ -1,8 +1,31 @@
 package model;
 
-public class MM1 extends FileAttente{
+public class MM1 extends FileAttente {
 
     MM1() {
         super();
+        nbServer = 1;
     }
+
+    public float computeRho() {
+        return this.lambda/this.mu;
+    }
+
+    public float computeNbCustomerInSystem() {
+        return this.lambda/(this.mu-this.lambda);
+    }
+
+    public float computeNbCustomerInQueue() {
+        return (this.lambda*this.lambda)/(this.mu*(this.mu-this.lambda));
+    }
+
+    public float computeMeanTimeInSystem() {
+        return 1/(this.mu-this.lambda);
+    }
+
+    public float computeMeanTimeInQueue() {
+        return this.lambda/(this.mu*(this.mu-this.lambda));
+    }
+
+
 }
