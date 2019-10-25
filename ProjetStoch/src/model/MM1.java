3ab819458fa91bfe5f12be14535db060ecb36432
CLacaile <clementlacaile@gmail.com>
2019-10-25 14:32:26 +0200

    added public to the constructor + added computeNbCustomerProbabilities + computeProbabilityOfStayingInSystem

--- ProjetStoch/src/model/MM1.java
+++ ProjetStoch/src/model/MM1.java
@@ -1,8 +1,10 @@
 package model;
 
+import java.util.List;
+
 public class MM1 extends FileAttente {
 
-    MM1() {
+    public MM1() {
         super();
         S = 1;
     }
@@ -27,5 +29,24 @@ public class MM1 extends FileAttente {
         return lambda/(mu*(mu-lambda));
     }
 
+    /**
+     *  Compute the probabilities of having i customer in the system, i being the index of the list
+     * @return the list of probabilities
+     */
+    public List<Float> computeNbCustomerProbabilities(int max) {
+        // Calcul de q
+        for(int i = 0; i<max; i++) {
+            q.set(i, (float) (Math.pow(rho, i)*(1-rho)));
+        }
+        return q;
+    }
 
+    /**
+     * Compute the probability for a person to stay longer than a time t (in unit of time)
+     * @param t
+     * @return Float
+     */
+    public Float computeProbabilityOfStayingInSystem(int t) {
+        return (float) Math.exp(-mu*(1 - rho)*t);
+    }
 }
