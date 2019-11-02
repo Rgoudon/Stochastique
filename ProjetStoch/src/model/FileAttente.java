a90940b81be2e474d9b7b974eaab0c933d12b695
CLacaile <clementlacaile@gmail.com>
2019-11-02 13:35:53 +0100

    added timeunit attribute + formated getMeanTime functions so it reflects timeunit

--- ProjetStoch/src/model/FileAttente.java
+++ ProjetStoch/src/model/FileAttente.java
@@ -1,24 +1,29 @@
 package model;
 
-import javafx.beans.property.FloatProperty;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleFloatProperty;
-import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileAttente {
-    protected FloatProperty lambda = new SimpleFloatProperty(0);     // Nombre de clients par unité de temps
-    protected FloatProperty mu = new SimpleFloatProperty(0);         // Nombre de services par unité de temps
-    protected IntegerProperty S = new SimpleIntegerProperty(0);            // Nombre de services (S)
-    protected IntegerProperty K = new SimpleIntegerProperty(0);             // Nombre maximum de clients dans le système (K)
-    protected FloatProperty rho = new SimpleFloatProperty(0);          // ratio lamdba/S*mu
-    protected FloatProperty L = new SimpleFloatProperty(0);            // nombre de clients dans le système
-    protected FloatProperty Lq = new SimpleFloatProperty(0);           // nombre de clients dans la file
-    protected FloatProperty W = new SimpleFloatProperty(0);            // temps d'attente moyen dans le système
-    protected FloatProperty Wq = new SimpleFloatProperty(0);           // temps d'attentee moyen dans la file
-    protected List<FloatProperty> q = new ArrayList<>();                        // P(Xt=i) : probabilité qu'il y ai i clients
+    protected FloatProperty lambda = new SimpleFloatProperty(0);          // Nombre de clients par unité de temps
+    protected FloatProperty mu = new SimpleFloatProperty(0);              // Nombre de services par unité de temps
+    protected IntegerProperty S = new SimpleIntegerProperty(0);           // Nombre de services (S)
+    protected IntegerProperty K = new SimpleIntegerProperty(0);           // Nombre maximum de clients dans le système (K)
+    protected FloatProperty rho = new SimpleFloatProperty(0);             // ratio lamdba/S*mu
+    protected FloatProperty L = new SimpleFloatProperty(0);               // nombre de clients dans le système
+    protected FloatProperty Lq = new SimpleFloatProperty(0);              // nombre de clients dans la file
+    protected FloatProperty W = new SimpleFloatProperty(0);               // temps d'attente moyen dans le système
+    protected FloatProperty Wq = new SimpleFloatProperty(0);              // temps d'attentee moyen dans la file
+    protected List<FloatProperty> q = new ArrayList<>();                             // P(Xt=i) : probabilité qu'il y ai i clients
+    protected StringProperty timeUnit = new SimpleStringProperty();                  // Unité de temps des calculs
+
+    /*public void initTimeUnits() {
+        timeUnits.put("ms", "Milliseconde");
+        timeUnits.put("s", "Seconde");
+        timeUnits.put("m", "Minute");
+        timeUnits.put("h", "Heure");
+    }*/
 
     public FloatProperty getLambda() {
         return lambda;
@@ -64,16 +69,23 @@ public class FileAttente {
         return Lq;
     }
 
-    public FloatProperty getMeanTimeInSystem() {
-        return W;
+    public StringProperty getMeanTimeInSystem() {
+        return new SimpleStringProperty(Utils.format(W.getValue(), timeUnit.getValue()));
     }
 
-    public FloatProperty getMeanTimeInQueue() {
-        return Wq;
+    public StringProperty getMeanTimeInQueue() {
+        return new SimpleStringProperty(Utils.format(Wq.getValue(), timeUnit.getValue()));
     }
 
     public List<FloatProperty> getProbabilityOfStates() {
         return q;
     }
 
+    public StringProperty getTimeUnit() {
+        return timeUnit;
+    }
+
+    public void setTimeUnit(StringProperty tu) {
+        timeUnit = tu;
+    }
 }
